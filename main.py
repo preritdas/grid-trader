befbdf8424eb6e78d00fc118229644b7a45aeba2
Prerit Das <preritdas@gmai.com>
2022-04-10 20:48:05 -0700

    First functional test using Coinbase BTCUSD.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import alpaca_trade_api
 
 # Local imports
 import multiprocessing as mp
+import time
 
 # Project modules
 import _keys
@@ -23,11 +24,17 @@ class GridTrader:
 
     It's recommended to deploy Grid Traders using multiprocessing. 
     """
-    def __init__(self, symbol: str, trading_range: tuple, grids_amount: int, account_allocation: float):
+    def __init__(self, symbol: str, trading_range: tuple, grids_amount: int, 
+                account_allocation: float, asset_class: str = 'stock'):
         # Exception if trading_range items aren't numbers
         for item in trading_range:
             if type(item) not in [int, float]:
                 raise Exception("Items in the trading_range tuple must be numbers.")
+
+        # Exception if invalid asset class is given
+        self.asset_class = asset_class.lower()
+        if self.asset_class != 'stock' and self.asset_class != 'crypto':
+            raise Exception("Invalid asset class given.")
 
         self.symbol = symbol
         self.range_bottom = trading_range[0]
@@ -44,17 +51,15 @@ class GridTrader:
             grid = self.range_bottom + (i * distance)
             self.grids.append(round(grid, 2))
 
-        # # !!!!!!!!!!!!!! POSSIBLY UNNECESSARY Dictionary with grid states (untouched, bought, sold)
-        # self.grid_states = {}
-        # for grid in self.grids:
-        #     self.grid_states[grid] = None
-
         # Define self.grids_below to allow first trade_logic iteration 
-        self.grids_below = []
+        self.grids_below = None
     
     def current_price(self):
         """Returns the current price of self.symbol, calculated by the latest trade."""
-        return float(alpaca.get_snapshot(symbol = self.symbol).latest_trade.p)
+        if self.asset_class == 'crypto':
+            return float(alpaca.get_latest_crypto_trade(symbol = self.symbol, exchange = 'CBSE').p)
+        else:
+            return float(alpaca.get_snapshot(symbol = self.symbol).latest_trade.p)
 
     def place_order(self, direction: str, size: int):
         """
@@ -65,16 +70,12 @@ class GridTrader:
         if direction != 'buy' and direction != 'sell':
             raise Exception("Invalid argument for direction. Must be 'buy' or 'sell'.")
 
-        # Place order with multiprocessing
-        def submit_order():
-            alpaca.submit_order(
-                symbol = self.symbol,
-                notional = size * float(alpaca.get_acccount().equity) * self.position_size,
-                side = direction
-            )
-
-        # Submit the order with multiprocess 
-        mp.Process(target = submit_order).start()
+        # alpaca.submit_order(
+        #     symbol = self.symbol,
+        #     notional = size * float(alpaca.get_acccount().equity) * self.position_size,
+        #     side = direction
+        # )
+        print(f"{direction = }, {size = }")
 
     def trade_logic(self):
         """
@@ -88,24 +89,40 @@ class GridTrader:
         current_price = self.current_price()
         # End logic/return nothing if price isn't in the grids
         if current_price < self.range_bottom or current_price > self.range_top:
+            print('Not in range.')
             return None
         
         # Calculate grids below 
         grids_below = [grid for grid in self.grids if grid < current_price]
+        print(grids_below)
+
+        # First iteration check
+        if self.grids_below is None:
+            self.grids_below = grids_below
 
         # Make a purchase decision
         if len(grids_below) > len(self.grids_below):
-            self.place_order('buy', len(grids_below) - len(self.grids_below))
+            order_args = ('buy', len(grids_below) - len(self.grids_below))
+            mp.Process(target = self.place_order, args = order_args).start()
         elif len(grids_below) < len(self.grids_below):
-            self.place_order('sell', len(self.grids_below) - len(grids_below))
+            order_args = ('sell', len(self.grids_below) - len(grids_below))
+            mp.Process(target = self.place_order, args = order_args).start()
+        else:
+            print('waiting')
 
         # Store grids below for next iteration
         self.grids_below = grids_below
 
     def deploy(self):
-        """Deploys the Grid Trader."""
-        # TESTING (run trade logic once instantly)
-        self.trade_logic()
+        """
+        Deploys the Grid Trader.
+        Iterates trade_logic during market hours once per second due to rate limit.
+        """
+        while True:
+            # if market is open:
+            self.trade_logic()
+            # time.sleep(0.61)
+
 
 if __name__ == "__main__":
-    GridTrader('AAPL', (150, 200), 11, 0.5).deploy()
+    GridTrader('BTCUSD', (42000, 42100), 11, 0.5, 'crypto').deploy()
